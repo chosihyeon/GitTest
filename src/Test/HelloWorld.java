e03ce434e278dbeaa7474a1ee539d1782e9c0925
chosihyeon <162430886+chosihyeon@users.noreply.github.com>
2024-05-13 19:42:11 +0900

    Update HelloWorld.java
    test

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Git world~~~~");
 		System.out.println("깃허브연동성공~~했음");
-
+		System.out.print("ddddddddd");
 	}
 
 }
